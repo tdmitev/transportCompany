1ac4193962994d2ef7ddf6bccdbf8390a81c022f
Todor Mitev <td_mitev@abv.bg>
2024-12-10 13:21:09 +0200

    Initialize java spring app - transport company

---
+++ src/main/java/com/example/transportCompany/TransportCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.example.transportCompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportCompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/transportCompany/TransportCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transportCompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
